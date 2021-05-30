12804cabb5ada5ed469ec938ecd986e7a17927d3
Dominic Prior <dominicprior42@gmail.com>
2021-05-30 14:37:05 +0100

    Draw a small function

--- bubbles.js
+++ bubbles.js
@@ -2,11 +2,42 @@
 
 // See https://esprima.readthedocs.io/en/latest/lexical-analysis.html
 
-let sq = `
-function sq(x) {
-  return x * x;  // hey
+let f = `
+function quad(a, b, c) {
+  const discr = b * b - 4 * a * c;
+  return (- b + sqrt(discr)) / (2 * a)
 }
 `
 
-let syntax = esprima.tokenize(sq, {range: true, comment: true });
-console.log(JSON.stringify(syntax, null, 4));
+let syntax = esprima.tokenize(f, {range: true, comment: true });
+
+let prevValue = ''
+let currDiv = document.createElement('div');
+for (let tok of syntax) {
+  const tokType = tok.type.toLowerCase();
+  console.log(tok.type, tok.value, tok.range);
+  let span = document.createElement('span');
+  span.textContent = tok.value
+  span.classList.add('padding');
+  if (prevValue === 'function') {
+    span.classList.add('functionName');
+  }
+  else {
+    span.classList.add(tokType);
+  }
+  if (tok.value === 'function' ||
+      tok.value === '{' || tok.value === '}' || tok.value === ';' ||
+      tokType === 'linecomment') {
+    // hide them for now
+  }
+  else {
+    currDiv.append(span);
+    currDiv.insertAdjacentText('beforeend', ' ');
+  }
+  if (tok.value === '{' || tok.value === ';') {
+    document.body.append(currDiv)
+    currDiv = document.createElement('div');
+  }
+  prevValue = tok.value;
+}
+document.body.append(currDiv)
